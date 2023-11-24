c6976c992ded636b209f41963f642ee7260af585
BruceMT <MWB201712228@outlook.com>
2023-11-25 01:16:14 +0800

    2023-11-25 new update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,8 @@
               </div>
             </div>
             <div  class="login-info">
-
+                <p>上次登录时间：<span>2023-11-25</span></p>
+                <p>上次登录地点：<span>2023-11-25</span></p>
             </div>
       </el-card>
     </div>
@@ -32,6 +33,43 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.user{
+  padding-bottom: 20px;
+  margin-bottom: 20px;
+  border-bottom: 1px solid #ccc;
+  display:flex;
+  align-items: center;
+  img{
+    margin-right: 40px;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+  }
+  .userinfo{
+
+    .name{
+      font-size:32px;
+      margin-bottom: 10px;
+    }
+    .access{
+      color: gray;
+    }
+  }
+}
+
+.login-info{
+  p{
+    line-height: 28px;
+    font-size: 14px;
+    color: #999999;
+    span{
+      color: #666666;
+      margin-left: 60px;
+    }
+  }
+}
+
+
 
 </style>
