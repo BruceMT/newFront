60f5a5d29fe55f5ea3b46a5931c4f225bea55ac9
MWB <brucemwb2018@gmail.com>
2023-12-04 16:54:02 +0800

    12/4 update

--- src/components/CommonTag.vue
+++ src/components/CommonTag.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="tabs">
     <el-tag
-        v-for="item in tags" :key="item.path"
+        v-for="(item,index) in tags" :key="item.path"
         :closable="item.name!='home'"
-        :effect="$route.name === item.name? 'dark' : 'plain'">
+        :effect="$route.name === item.name? 'dark' : 'plain'"
+        @click="changeMenu(item)"
+        @close="handleClose(item,index)"
+    >
       {{ item.label }}
     </el-tag>
   </div>
 </template>
 
 <script>
-import{mapState} from 'vuex'
+import{mapState,mapMutations} from 'vuex'
 
 export default {
   name: "CommonTag",
+  methods:{
+    //将tab。js里的删除方法解构出来
+    ...mapMutations(['closeTag']),
+    changeMenu(item){
+      this.$router.push({name:item.name})
+    },
+    handleClose(item,index){
+      //调用store中的mutation
+      this.closeTag(item)
+
+      const length = this.tags.length
+      //删除之后跳转
+      if(item.name!== this.$route.name){
+        return
+      }
+      //删除的是最后一项
+      if(index === length){
+        this.$router.push({
+          name:this.tags[index-1].name
+        })
+      }else {
+        this.$router.push({
+          name:this.tags[index].name
+        })
+      }
+    }
+  },
   computed:{
     //解构并且获取vuex中的数据
     ...mapState({
